1786d59f85156b183ae20e90a2f4aca043ebb9fb
Gabriela Porto <gporto@imersys.com>
2021-01-22 19:18:34 -0300

    Reta desenhada e coordenadas mouse endo pegas

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 function draw() {
     var canvas = document.getElementById('tutorial');
+    canvas.style="border:solid;border-size:2px;border-radius:10px"
 
+    canvas.width = $( window ).width()
+    canvas.height = $( window ).height()
     if(canvas.getContext){
         var ctx = canvas.getContext('2d');
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 55, 50);
 
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
+    var a = ctx.beginPath();
+    var b = ctx.moveTo(($( window ).width()/3), $( window ).height()/4);
+    var c = ctx.lineTo($( window ).width()/2, $( window ).height()/2);
+    var d = ctx.stroke();
+
+    console.log(a,b,c,d)
     }
     else{
         console.log('erro')
     }
+
+    console.log('jdiso')
+    canvas.click((event) => {
+        var offset = $(this).offset()
+        console.log(offset)
+        console.log(event.clientX)
+        console.log(event.clientY)
+    })
+
+    $('#tutorial').click(function (event) { //Default mouse Position 
+        alert(event.pageX + ' , ' + event.pageY);
+    });
 }
 
+$(document).ready(() => {
+    
+})
